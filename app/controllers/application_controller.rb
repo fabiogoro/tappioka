e4cfa51d71a425775e849493d05177c84a30bbc4
Fábio Goródscy <fabiogorodscy@hotmail.com>
2016-02-09 00:14:16 -0200

    User views categories on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to :html
+  before_action :set_categories, only: [:show, :index]
+
+  def set_categories
+    @cuisines = Cuisine.all
+    @types = Type.all
+    @preferences = Preference.all
+  end
 end
--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -21,4 +21,43 @@ feature 'User visits homepage' do
 
     expect(page).to have_selector 'a.recipe-main', count: 20
   end
+
+  scenario 'and lists jobs by cuisines' do
+    cuisine = create(:cuisine, name: "Mexicana")
+    recipe = create(:recipe, name: "Nacho", cuisine: cuisine)
+    another_recipe = create(:recipe)
+
+    visit root_path
+    click_on cuisine.name
+
+    expect(page).to have_content cuisine.name
+    expect(page).to have_content recipe.name
+    expect(page).not_to have_content another_recipe.name
+  end
+
+  scenario 'and lists jobs by types' do
+    type = create(:type, name: "Salada")
+    recipe = create(:recipe, name: "Salada de alface", type: type)
+    another_recipe = create(:recipe)
+
+    visit root_path
+    click_on type.name
+
+    expect(page).to have_content type.name
+    expect(page).to have_content recipe.name
+    expect(page).not_to have_content another_recipe.name
+  end
+
+  scenario 'and lists jobs by preferences' do
+    preference = create(:preference, name: "Carnes")
+    recipe = create(:recipe, name: "Bife acebolado", preference: preference)
+    another_recipe = create(:recipe)
+
+    visit root_path
+    click_on preference.name
+
+    expect(page).to have_content preference.name
+    expect(page).to have_content recipe.name
+    expect(page).not_to have_content another_recipe.name
+  end
 end
